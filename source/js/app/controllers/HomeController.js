8ccc4a418b89fcec27ea0264b5eb6eb234cbcac0
Jordan Roskelley <jroskelley@dsdmail.net>
2015-04-28 09:21:16 -0600

    Tweaked styling, add, remove, copy bulletins

--- source/js/app/controllers/HomeController.js
+++ source/js/app/controllers/HomeController.js
@@ -1,4 +1,4 @@
-function HomeController($scope, dataService) {
+function HomeController($scope, Data) {
 	function init() {
 		Data.init();
 		$scope.d = Data.data;
@@ -15,12 +15,31 @@ function HomeController($scope, dataService) {
 			$scope.d = [];
 		}
 
+		var now = new Date();
+		var dateStr = (now.getMonth()+1) + "/" + (now.getDate()) + "/" + (now.getFullYear())
+
 		//add an empty bulletin to the list
-		$scope.d.push({});
+		$scope.d.push({date: dateStr, status: 'new'});
 	};
 
-	$scope.removeBulletin = function (index) {
-		//remove a bulletin from the list
-		$scope.d.splice(index, 1)
+	$scope.save = function (index) {
+		$scope.d[index].status = null;
 	};
+
+	$scope.delete = function (index) {
+		//remove a bulletin from the list
+		$scope.d.splice(index, 1);
+	};
+
+	$scope.copy = function (index) {
+		console.log('copy ' + index);
+		var copy = angular.copy($scope.d[index]);
+		$scope.d.push(copy);
+	};
+
+	$scope.edit = function (index) {
+		console.log('edit ' + index);
+	};
+
+	init();
 }
